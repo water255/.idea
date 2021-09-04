95045044b5fe6ba876bd36e4ee5905d8d7b6f151
김성민 <kimsungmin@gimseongmin-ui-MacBookAir.local>
2021-09-04 13:59:23 +0900

    Initial commit

---
+++ src/main/Variable.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		byte sungsam = 0;
+		String output = "I have" + sungsam + "cookies";
+		System.out.println(output);
+	}
+
+}
+
---
+++ src/main/practice.java
@@ -0,0 +1,15 @@
+package main;
+
+public class practice {
+
+	public static void main(String[] args) {
+		int num = 5;
+		String output = "I have " + num + " cookies";
+		System.out.println(output);
+
+	}
+
+	
+}
+
+
